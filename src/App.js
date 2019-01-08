4a828df09ee9631fca4f195e2cdb39688d9b7383
Cheulong Sear <long@buzzwoo.de>
2019-01-08 12:43:48 +0700

    redux

--- src/App.js
+++ src/App.js
@@ -1,108 +1,34 @@
 import React from "react";
-import { render } from "react-dom";
-import { Router } from "@reach/router";
-import pf from "petfinder-client";
-import Loadable from "react-loadable";
-import { Provider } from "./SearchContext";
-import NavBar from "./NavBar";
-
-const petfinder = pf({
-  key: "70314776187bbcb072e6ec8c07e6cf6c",
-  secret: "8cd79232ce57a5fd9532e25ea66e1279"
-});
-
-const LoadableDetails = Loadable({
-  loader: () => import("./Details"),
-  loading() {
-    return <h1>loading split out code</h1>;
-  }
-});
-
-const LoadableSearchParams = Loadable({
-  loader: () => import("./SearchParams"),
-  loading() {
-    return <h1>loading split out code</h1>;
-  }
-});
-
-const LoadableResults = Loadable({
-  loader: () => import("./Results"),
-  loading() {
-    return <h1>loading split out code</h1>;
-  }
-});
+import ReactDOM from "react-dom";
+import { Router, Link } from "@reach/router";
+import { Provider } from "react-redux";
+import store from "./store";
+import Results from "./Results";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: "Seattle, WA",
-      animal: "",
-      breed: "",
-      breeds: [],
-      handleLocationChange: this.handleLocationChange,
-      handleBreedChange: this.handleBreedChange,
-      handleAnimalChange: this.handleAnimalChange,
-      getBreeds: this.getBreeds
-    };
-  }
-  handleLocationChange = event => {
-    this.setState({
-      location: event.target.value
-    });
-  };
-  handleAnimalChange = event => {
-    this.setState(
-      {
-        animal: event.target.value
-      },
-      this.getBreeds
-    );
-  };
-  handleBreedChange = event => {
-    this.setState({
-      breed: event.target.value
-    });
-  };
-  getBreeds() {
-    if (this.state.animal) {
-      petfinder.breed
-        .list({ animal: this.state.animal })
-        .then(data => {
-          if (
-            data.petfinder &&
-            data.petfinder.breeds &&
-            Array.isArray(data.petfinder.breeds.breed)
-          ) {
-            this.setState({
-              breeds: data.petfinder.breeds.breed
-            });
-          } else {
-            this.setState({ breeds: [] });
-          }
-        })
-        // eslint-disable-next-line no-console
-        .catch(console.error);
-    } else {
-      this.setState({
-        breeds: []
-      });
-    }
-  }
   render() {
     return (
-      <div>
-        <NavBar />
-        <Provider value={this.state}>
+      <Provider store={store}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+            <Link to="/search-params">
+              <span aria-label="search" role="img">
+                🔍
+              </span>
+            </Link>
+          </header>
           <Router>
-            <LoadableResults path="/" />
-            <LoadableDetails path="/details/:id" />
-            <LoadableSearchParams path="/search-params" />
+            <Results path="/" />
+            <Details path="/details/:id" />
+            <SearchParams path="/search-params" />
           </Router>
-        </Provider>
-      </div>
+        </div>
+      </Provider>
     );
   }
 }
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
